dfc2bd5c2fef6700732f2a4b1442647c9e9ea4fa
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-21 15:29:36 +0900

    [Script push]

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+val project_version: String by project
+val jdk_version: String by project
+val kotlinx_coroutines_version: String by project
+val kotlinx_datetime_version: String by project
+val kotlinx_serialization_version: String by project
+val hcmc_extension_version: String by project
+val ktor_version: String by project
+val exposed_version: String by project
+
+plugins {
+    kotlin("jvm")
+    id("maven-publish")
+}
+
+group = "studio.hcmc"
+version = project_version
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    maven { setUrl("https://jitpack.io") }
+}
+
+java {
+    withSourcesJar()
+}
+
+kotlin {
+    jvmToolchain(jdk_version.toInt())
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "studio.hcmc"
+            artifactId = project.name
+            version = project_version
+            from(components["java"])
+        }
+        create<MavenPublication>("jitpack") {
+            groupId = "com.github.hcmc-studio"
+            artifactId = project.name
+            version = "$project_version-release"
+            from(components["java"])
+        }
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:$kotlinx_serialization_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:$kotlinx_serialization_version")
+
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+}
---
+++ src/main/kotlin/studio/hcmc/ktor/plugin/JsonContentNegotiation.kt
@@ -0,0 +1,30 @@
+package studio.hcmc.ktor.plugin
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.util.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonBuilder
+
+private val defaultJsonKey = AttributeKey<Json>("defaultJson")
+
+val Application.defaultJson: Json get() = attributes[defaultJsonKey]
+
+class JsonContentNegotiationConfiguration(
+    var jsonBuilder: JsonBuilder.() -> Unit = {},
+    var contentNegotiationConfiguration: ContentNegotiationConfig.() -> Unit = {}
+)
+
+val JsonContentNegotiation = createApplicationPlugin("JsonContentNegotiation", ::JsonContentNegotiationConfiguration) {
+    val json = Json {
+        pluginConfig.jsonBuilder(this)
+    }
+
+    application.attributes.put(defaultJsonKey, json)
+
+    application.install(ContentNegotiation) {
+        json(json)
+        this@createApplicationPlugin.pluginConfig.contentNegotiationConfiguration(this)
+    }
+}
